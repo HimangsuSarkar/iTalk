f4049f86e4f976d5d6e48a9daade16279fa44d93
HimangsuSarkar <himangsusarkar1@gmail.com>
2021-05-28 22:47:31 +0800

    set up react-redux app

---
+++ src/Imessage/Imessage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Imessage.css";
+
+function Imessage() {
+    return (
+        <div className="imessage">
+            {/*SideBar*/}
+
+
+            {/*Chat*/}
+
+        </ div>
+    )
+}
+
+export default Imessage
+
