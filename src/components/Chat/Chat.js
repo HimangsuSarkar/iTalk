2ee68cfe8f64eb627c696fd876c5af4eb82fd784
HimangsuSarkar <himangsusarkar1@gmail.com>
2021-05-29 09:25:50 +0800

    chat body and chat input section done

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import { IconButton } from '@material-ui/core';
+import { MicNone } from '@material-ui/icons';
+import React, { useState } from 'react'
 import "./Chat.css";
 
 function Chat() {
+    const [input, setInput] = useState("");
+    const sendMessage = (e) => {
+        e.preventDefault();
+
+        setInput("");
+    };
+
     return (
         <div className="chat">
             {/*chat header*/}
@@ -11,7 +20,23 @@ function Chat() {
             </div>
             {/*chat body*/}
 
-            {/*chat input*/}
+
+            <div className="chat_message">
+                <h2>I am a message</h2>
+            </div>
+
+            <div className="chat_input">
+                <form action="">
+                    <input value={input}
+                        onChange={(e) => setInput(e.target.value)}
+                        type="text" placeholder="iMessage" />
+                    <button type="submit" onClick={sendMessage}>Send Message</button>
+                </form>
+
+                <IconButton>
+                    <MicNone className="chat_mic" />
+                </IconButton>
+            </div>
         </ div >
     )
 }
